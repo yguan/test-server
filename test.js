2d3dc4e25d938f6592bcea4a06bb90737299e82f
Yong Guan <gyong9@gmail.com>
2014-05-10 20:44:37 -0700

    raw working code

--- test.js
+++ test.js
@@ -1,34 +1,61 @@
-var restify = require('restify');
+var restify = require('restify'),
+    testServer = require('./lib/test-server'),
+    serverMode = require('./lib/server-mode'),
+    targetServerPort = 9005,
+    targetServerUrl = 'http://localhost:' + targetServerPort;
 
-function respond(req, res, next) {
-    res.send('hello ' + req.params.name);
-    next();
+function createTargetServer () {
+    function respond(req, res, next) {
+        res.send('hello ' + req.params.name);
+        next();
+    }
+
+    var server = restify.createServer({
+        name: 'myapp',
+        version: '1.0.0'
+    });
+    server.use(restify.acceptParser(server.acceptable));
+    server.use(restify.queryParser());
+    server.use(restify.bodyParser());
+
+    server.get('/hello/:name', respond);
+    server.head('/hello/:name', respond);
+
+    function send(req, res, next) {
+        res.send(201, 'hello ' + req.params.name);
+        return next();
+    }
+
+    server.post('/post', send);
+
+//    server.post('/post', function create(req, res, next) {
+//        res.send(201, Math.random().toString(36).substr(3, 8));
+//        return next();
+//    });
+//    server.put('/hello', send);
+//    server.get('/hello/:name', send);
+//    server.head('/hello/:name', send);
+//    server.del('hello/:name', function rm(req, res, next) {
+//        res.send(204);
+//        return next();
+//    });
+
+    server.listen(targetServerPort, function() {
+        console.log('%s target server listening at %s', server.name, server.url);
+    });
 }
 
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+function createTestServer () {
+    testServer.createServer({
+        mode: serverMode.passive,
+        port: 9006,
+        targetServerUrl: targetServerUrl,
+        databaseDirectory: 'C:\\Users\\coding\\Documents\\GitHub\\test-server\\db-files\\'
+    });
+}
+createTargetServer();
+createTestServer();
 
-server.listen(8080, function() {
-    console.log('%s listening at %s', server.name, server.url);
-});
+// test: http://localhost:9006/hello/some
 
-//
-//
-//var Datastore = require('nedb'),
-//    db = new Datastore({ filename: 'test.txt', autoload: true });
-//
-//var doc = { hello: 'world'
-//    , n: 5
-//    , today: new Date()
-//    , nedbIsAwesome: true
-//    , notthere: null
-//    , notToBeSaved: undefined  // Will not be saved
-//    , fruits: [ 'apple', 'orange', 'pear' ]
-//    , infos: { name: 'nedb' }
-//};
-//
-//db.insert(doc, function (err, newDoc) {   // Callback is optional
-//    // newDoc is the newly inserted document, including its _id
-//    // newDoc has no key called notToBeSaved since its value was undefined
-//});
+
